b472c7e455a0bc790500eddec8a91c7d0ff0bbf6
gandharkulkarni <gakulkarni@dons.usfca.edu>
2024-02-29 22:40:58 -0800

    Rendering router component

--- src/client/classroom-finder/screen.js
+++ src/client/classroom-finder/screen.js
@@ -3,19 +3,20 @@ import {
   BannerHeader,
   Panel,
   HBox,
-  AekReactRouter,
-  RouterView,
+  //   AekReactRouter,
+  //   RouterView,
   VBox
 } from "@ombiel/aek-lib";
 
 import styles from "./css/screen-styles.css";
+import Router from "./router";
 
-import HomePage from "./components/home-page";
-import NoResultsFound from "./components/no-results-found";
-import ClassrooomList from "./components/classroom-list";
-import SearchResults from "./components/search-results";
+// import HomePage from "./components/home-page";
+// import NoResultsFound from "./components/no-results-found";
+// import ClassrooomList from "./components/classroom-list";
+// import SearchResults from "./components/search-results";
 
-const router = new AekReactRouter({useHash: false});
+// const router = new AekReactRouter({useHash: false});
 
 
 export default class Screen extends React.Component {
@@ -32,15 +33,9 @@ export default class Screen extends React.Component {
           <BannerHeader key="header" className={styles.screenTitle} data-flex={0}>Classroom Finder</BannerHeader>
           <Panel>
             <HBox>
-              <RouterView router={router}>
-                <HomePage path="/" {...{router}} />
-                <SearchResults path="/search/:keyword" {...{router}} />
-                <NoResultsFound path="/search/no-results/:keyword" {...{router}} />
-                <ClassrooomList path="/classroom/:building" {...{router}} />
-              </RouterView>
+              <Router />
             </HBox>
           </Panel>
-          
         </VBox>
       </Panel>
     );
