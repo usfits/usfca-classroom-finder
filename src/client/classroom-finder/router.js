71e7b4e659205030e955eefde14aa74f4b4e38ee
gandharkulkarni <gakulkarni@dons.usfca.edu>
2024-02-29 22:40:36 -0800

    implemented router layer at the top

--- src/client/classroom-finder/router.js
+++ src/client/classroom-finder/router.js
@@ -1,4 +1,5 @@
-import React from "react";
+import React, {useEffect, useState} from "react";
+import axios from "axios";
 import {
   AekReactRouter,
   RouterView
@@ -6,13 +7,38 @@ import {
 
 import HomePage from "./components/home-page";
 import NoResultsFound from "./components/no-results-found";
+import ClassrooomList from "./components/classroom-list";
+import SearchResults from "./components/search-results";
+import { API } from "./constants";
 const router = new AekReactRouter({useHash: false});
 
 export default function Router() {
+
+  const [masterData, setMasterData] = useState(null);
+  
+  const fetchMasterData = async () =>{
+    try {
+      const response = await axios.get(API);
+      console.log(response.data.data);
+      setMasterData(response.data.data);
+    }
+    catch (error) {
+      console.log(error);
+    }
+  };
+  
+  useEffect(() => {
+    if (masterData === null) {
+      fetchMasterData();    
+    }
+  }, [masterData]);
+    
   return (
     <RouterView router={router}>
-      <HomePage path="/" />
-      <NoResultsFound path="/search" />
+      <HomePage path="/" {...{router, masterData}} />
+      <SearchResults path="/search/:keyword" {...{router, masterData}} />
+      <NoResultsFound path="/search/no-results/:keyword" {...{router, masterData}} />
+      <ClassrooomList path="/classroom/:building" {...{router, masterData}} />
     </RouterView>
   );
 }
