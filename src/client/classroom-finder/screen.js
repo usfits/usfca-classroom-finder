1a6cddf5577bf163cb56868b3653753110deec7b
gandharkulkarni <gakulkarni@dons.usfca.edu>
2024-02-29 22:41:30 -0800

    Code cleanup

--- src/client/classroom-finder/screen.js
+++ src/client/classroom-finder/screen.js
@@ -3,20 +3,11 @@ import {
   BannerHeader,
   Panel,
   HBox,
-  //   AekReactRouter,
-  //   RouterView,
   VBox
 } from "@ombiel/aek-lib";
 
 import styles from "./css/screen-styles.css";
 import Router from "./router";
-
-// import HomePage from "./components/home-page";
-// import NoResultsFound from "./components/no-results-found";
-// import ClassrooomList from "./components/classroom-list";
-// import SearchResults from "./components/search-results";
-
-// const router = new AekReactRouter({useHash: false});
 
 
 export default class Screen extends React.Component {
