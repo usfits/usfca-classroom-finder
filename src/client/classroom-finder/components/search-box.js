850589e17e58a107b3a385f62b6da4ea0d33b8c9
gandharkulkarni <gakulkarni@dons.usfca.edu>
2024-02-29 17:26:48 -0800

    Code clean up

--- src/client/classroom-finder/components/search-box.js
+++ src/client/classroom-finder/components/search-box.js
@@ -6,7 +6,7 @@ import {
 import PropTypes from "prop-types";
 import commonStyles from "../css/common-styles.css";
 import homePageStyles from "../css/homepage-styles.css";
-import { buildingNameList} from "../constants";
+
 export default function SearchBox(props) {
 
   const [searchKeyword, setSearchKeyword] = useState('');
@@ -18,44 +18,6 @@ export default function SearchBox(props) {
     }
     props?.router?.goto(`#/search/${searchKeyword}`);
   }
-//   const [results, setResults] = useState([]);
-/*
-  const handleSearch = ()=>{
-
-    if (searchKeyword.length < 2) {
-        alert("Please enter at least 2 characters.");
-        return;
-    }
-
-    let searchResults = {};
-    let buildingResults = [];
-    let classroomResults = [];
-    for (let building of buildingNameList) {
-      if (building.name.toLowerCase().includes(searchKeyword.toLowerCase())) {
-        buildingResults.push(building.name);
-      }
-      for (let room of building.classrooms) {
-        if (room.toLowerCase().includes(searchKeyword.toLowerCase())) {
-            classroomResults.push(room);
-        }
-      }
-    }
-    
-    //TODO: Implement search building/classroom name
-    if (buildingResults.length!==0 || classroomResults.length!==0) {
-      //TODO: If building/classroom exists, show them results
-      searchResults["buildingResults"] = buildingResults;
-      searchResults["classroomResults"] = classroomResults;
-    //   setResults(searchResults);
-    //   console.log(searchResults);
-      props?.router?.goto(`#/search/${searchResults}`);
-    }
-    else {
-      props?.router?.goto(`#/search/${searchKeyword}`);
-    }
-        
-  };
-  */
   return (
     <div className={homePageStyles.searchBox}>
       <Input name="name" placeholder="Search..." fluid size="large">
