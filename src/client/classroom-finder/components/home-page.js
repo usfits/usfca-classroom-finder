af3279fb6980be12967fbf90ed450e519f72079a
gandharkulkarni <gakulkarni@dons.usfca.edu>
2024-02-29 22:43:13 -0800

    integrated api

--- src/client/classroom-finder/components/home-page.js
+++ src/client/classroom-finder/components/home-page.js
@@ -9,20 +9,24 @@ import RecentSearch from "./recent-search";
 import BuildingList from "./building-list";
 
 import SearchBox from "./search-box";
-
-
-
+import buildingStyles from "../css/building-styles.css";
+import commonStyles from "../css/common-styles.css";
 export default function HomePage(props) {
-  
-  
-
   return (
     <Page>
       <BasicSegment>
         <div>
-          <RecentSearch />
-      
-          <BuildingList {...props} />
+          <h4 className={commonStyles.textCenter}>Recent Search</h4>
+          <hr className={commonStyles.hr50} /><br />
+          <div className={buildingStyles.buildingListDiv}>
+            {/* <RecentSearch /> */}
+            <BuildingList {...props} />
+          </div>
+          <h4 className={commonStyles.textCenter}>Buildings & Classrooms</h4>
+          <hr className={commonStyles.hr50} /><br />
+          <div className={buildingStyles.buildingListDiv}>
+            <BuildingList {...props} />
+          </div>
 
           <SearchBox {...props} />
         </div>
--- src/client/classroom-finder/components/search-results.js
+++ src/client/classroom-finder/components/search-results.js
@@ -1,52 +1,61 @@
 import React, {useState} from "react";
 import PropTypes from "prop-types";
 import { Page, BasicSegment} from "@ombiel/aek-lib";
-import { buildingNameList} from "../constants";
+import HomePageButton from "./home-page-button";
+import BuildingButton from "./building-button";
+import ClassroomButton from "./classroom-button";
+import buildingStyles from "../css/building-styles.css";
+import commonStyles from "../css/common-styles.css";
 export default function SearchResults(props) {
   const [buildings, setBuildings] = useState([]);
   const [classrooms, setClassrooms] = useState([]);
   const searchKeyword = props.ctx.params.keyword;
-  
+  const masterData = props?.masterData;
   if(classrooms.length<1 && buildings.length<1){
   let buildingResults = [];
   let classroomResults = [];
-  for (const building of buildingNameList) {
-    if (building.name.toLowerCase().includes(searchKeyword.toLowerCase())) {
-      buildingResults.push(building.name);
+  if(masterData){
+  for (const building of masterData) {
+
+    if (building.building.toLowerCase().includes(searchKeyword.toLowerCase())) {
+      buildingResults.push(building.building);
     }
     for (const room of building.classrooms) {
-      if (room.toLowerCase().includes(searchKeyword.toLowerCase())) {
-        classroomResults.push(room);
+      if (room.room.toLowerCase().includes(searchKeyword.toLowerCase())) {
+        classroomResults.push(room.room);
       }
     }
   }
-  console.log(buildingResults);
-  console.log(classroomResults);
+}
   if(buildingResults.length===0 && classroomResults.length===0){
     props?.router?.goto(`#/search/no-results/${searchKeyword}`);
   } else{
     setBuildings(buildingResults);
     setClassrooms(classroomResults);
-    console.log("State set");
   }
 }
   return (
     <Page>
       <BasicSegment>
         <div>
-            {buildings.length>0 && <h1>Buildings</h1>}
+            {buildings.length>0 && <h1 className={commonStyles.textCenter}>Buildings</h1>}
           {buildings.length>0 && buildings.map((building, index)=>{
             return(
-                <h1 key={index}>{building}</h1>
+                <div className={buildingStyles.buildingList}>
+                    <BuildingButton key={index} buildingName={building} {...props} />
+                </div>
             );
           })}
-            {classrooms.length>0 && <h1>Classrooms</h1>}
+            {classrooms.length>0 && <h1 className={commonStyles.textCenter}>Classrooms</h1>}
             {classrooms.length>0 && classrooms.map((classroom, index)=>{
             return(
-                <h1 key={index}>{classroom}</h1>
+                <ClassroomButton key={index} classroomName={classroom} />
             );
           })}
 
+        </div>
+        <div className={commonStyles.textCenter}>
+            <HomePageButton />
         </div>
       </BasicSegment>
     </Page>
@@ -55,5 +64,6 @@ export default function SearchResults(props) {
 
 SearchResults.propTypes = {
   ctx: PropTypes.object,
+  masterData: PropTypes.array,
 };
   
