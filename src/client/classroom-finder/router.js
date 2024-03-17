cd3fa918edfe0fd3e05f26456513a0921cfcc10c
gandharkulkarni <gakulkarni@dons.usfca.edu>
2024-03-17 10:51:47 -0700

    implemented hashed userid

--- src/client/classroom-finder/router.js
+++ src/client/classroom-finder/router.js
@@ -2,8 +2,10 @@ import React, {useEffect, useState} from "react";
 import axios from "axios";
 import {
   AekReactRouter,
-  RouterView
+  RouterView,
+  request
 } from "@ombiel/aek-lib";
+import sha256 from "crypto-js/sha256";
 
 import HomePage from "./components/home-page";
 import NoResultsFound from "./components/no-results-found";
@@ -15,6 +17,7 @@ const router = new AekReactRouter({useHash: false});
 export default function Router() {
 
   const [masterData, setMasterData] = useState(null);
+  const [userid, setUserId] = useState(null);
   
   const fetchMasterData = async () =>{
     try {
@@ -26,19 +29,30 @@ export default function Router() {
       console.log(error);
     }
   };
+
+  const fetchUserId = () => {
+    request
+    .action("get-user", { sso: true })
+    .then((response) => {
+      setUserId(sha256(response.body.username).toString());
+    });
+  };
   
   useEffect(() => {
     if (masterData === null) {
       fetchMasterData();    
     }
+    if (userid === null) {
+      fetchUserId();
+    }
   }, [masterData]);
     
   return (
     <RouterView router={router}>
-      <HomePage path="/" {...{router, masterData}} />
+      <HomePage path="/" {...{router, masterData, userid}} />
       <SearchResults path="/search/:keyword" {...{router, masterData}} />
       <NoResultsFound path="/search/no-results/:keyword" {...{router, masterData}} />
-      <ClassrooomList path="/classroom/:building" {...{router, masterData}} />
+      <ClassrooomList path="/classroom/:building" {...{router, masterData, userid}} />
     </RouterView>
   );
 }
