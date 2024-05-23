dc9f9b1e1dfb5960ce3b386fa9df545a40f981fa
gandharkulkarni <gakulkarni@dons.usfca.edu>
2024-05-22 19:05:43 -0700

    Moved the api call to homepage

--- src/client/classroom-finder/components/home-page.js
+++ src/client/classroom-finder/components/home-page.js
@@ -1,8 +1,11 @@
 import React, { useEffect } from "react";
 import {
   Page,
-  BasicSegment
+  BasicSegment,
+  request,
 } from "@ombiel/aek-lib";
+import sha256 from "crypto-js/sha256";
+import axios from "axios";
 import PropTypes from "prop-types";
 import RecentSearch from "./recent-search";
 
@@ -11,15 +14,52 @@ import BuildingList from "./building-list";
 import SearchBox from "./search-box";
 import buildingStyles from "../css/building-styles.css";
 import commonStyles from "../css/common-styles.css";
-import {CAMPUSM_ASSETS_SANDBOX, HERO_IMAGE} from "../constants";
+import {CAMPUSM_ASSETS_SANDBOX, HERO_IMAGE, API} from "../constants";
+
 export default function HomePage(props) {
   const { masterData } = props;
+  const { userid } = props;
   const { classroomPage } = props;
+  const { setMasterData } = props;
+  const { setUserId } = props;
+  const { refreshCache } = props;
+  const { router } = props;
+
+
+  const fetchMasterData = async () =>{
+    try {
+      const response = await axios.get(`${API}/all`);
+      console.log(response.data.data);
+      setMasterData(data => response.data.data);
+      router.goto("#/");
+    }
+    catch (error) {
+      console.log(error);
+    }
+  };
+
+  const fetchUserId = () => {
+    request
+    .action("get-user", { sso: true })
+    .then((response) => {
+      const hashedId = sha256(response.body.username).toString();
+      setUserId(hashedId);
+      refreshCache(hashedId);
+    });
+  };
+  
+  useEffect(() => {
+    if (userid === null) {
+      fetchUserId();
+    }
+    if (masterData === null) {
+      fetchMasterData();  
+    }
+  }, [userid, masterData]);
 
   useEffect(() => {
     if (masterData !== null) {
       if (classroomPage.classroomPage !== '') {
-        const { router } = props;
         router.goto(`#/classroom-detail/${classroomPage}`);
       }
     }
@@ -49,5 +89,9 @@ export default function HomePage(props) {
 HomePage.propTypes = {
   router: PropTypes.object,
   masterData: PropTypes.array,
-  classroomPage: PropTypes.object
+  userid: PropTypes.string,
+  classroomPage: PropTypes.object,
+  setMasterData: PropTypes.func,
+  setUserId: PropTypes.func,
+  refreshCache: PropTypes.func,
 };
--- src/client/classroom-finder/router.js
+++ src/client/classroom-finder/router.js
@@ -1,11 +1,9 @@
-import React, {useEffect, useState} from "react";
+import React, {useState} from "react";
 import axios from "axios";
 import {
   AekReactRouter,
   RouterView,
-  request
 } from "@ombiel/aek-lib";
-import sha256 from "crypto-js/sha256";
 
 import HomePage from "./components/home-page";
 import NoResultsFound from "./components/no-results-found";
@@ -19,20 +17,6 @@ export default function Router(classroomPage) {
   const [masterData, setMasterData] = useState(null);
   const [userid, setUserId] = useState(null);
   const [recent, setRecent] = useState(null);
-  const [dataLoaded, setDataLoaded] = useState(false);
-  
-  const fetchMasterData = async () =>{
-    try {
-      const response = await axios.get(`${API}/all`);
-      console.log(response.data.data);
-      setMasterData(data => response.data.data);
-      setDataLoaded(data => true);
-      router.goto("#/");
-    }
-    catch (error) {
-      console.log(error);
-    }
-  };
 
   const refreshCache = async (id) => {
     const response = await axios.get(`${API}/classroom`, {params: { userid: id }});
@@ -40,28 +24,9 @@ export default function Router(classroomPage) {
     console.log("Cache refreshed");
   };
 
-  const fetchUserId = () => {
-    request
-    .action("get-user", { sso: true })
-    .then((response) => {
-      const hashedId = sha256(response.body.username).toString();
-      setUserId(hashedId);
-      refreshCache(hashedId);
-    });
-  };
-  
-  useEffect(() => {
-    if (userid === null) {
-      fetchUserId();
-    }
-    if (masterData === null) {
-      fetchMasterData();  
-    }
-  }, [userid, masterData]);
-
   return (
-    <RouterView router={router} key={dataLoaded}>
-      <HomePage path="/" {...{router, masterData, userid, recent, refreshCache, classroomPage}} />
+    <RouterView router={router}>
+      <HomePage path="/" {...{router, masterData, userid, recent, refreshCache, classroomPage, setMasterData, setUserId}} />
       <SearchResults path="/search/:keyword" {...{router, masterData}} />
       <NoResultsFound path="/search/no-results/:keyword" {...{router, masterData}} />
       <ClassrooomList path="/classroom/:building" {...{router, masterData, userid, refreshCache}} />
